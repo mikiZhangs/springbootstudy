655eee0ab33c6299ac9805b33c5eb742bd008e20
min_zhang <min_zhang@intsig.net>
2022-09-14 16:44:50 +0800

    [WN] init

---
+++ src/test/java/com/qxb/LdpApplicationTest.java
@@ -0,0 +1,18 @@
+package com.qxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zhangmin
+ * @version 1.0.0
+ * @company 合合信息
+ * @createTime 2022/9/14 16:43
+ */
+@SpringBootTest
+public class LdpApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
